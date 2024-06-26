3544b37b82473f75d973e9d46b521344241b1154
Fabio Flores Regueira <fabio.flores.regueira@gmail.com>
2024-06-26 18:54:09 -0300

    Leiaute com TimeLine

--- vianodejs/MonitoraTags/lib/main.dart
+++ vianodejs/MonitoraTags/lib/main.dart
@@ -26,13 +26,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TagsApp',
+      title: 'MonitoraTags',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Tags de Monitoramento'),
+      home: const MyHomePage(title: 'Monitorar Vigencias'),
     );
   }
 }
@@ -67,10 +67,217 @@ class _MyHomePageState extends State<MyHomePage> {
 
           if (snapshot.connectionState == ConnectionState.done) {
             return ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return Card(
+                padding: const EdgeInsets.all(8),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      /**TIMELINE**/
+                      TimelineTile(
+                        isFirst: index == 0,
+                        isLast: index == snapshot.data!.length - 1,
+                        indicatorStyle: IndicatorStyle(
+                          width: 30,
+                          //color: Colors.deepPurpleAccent,
+                          color: (snapshot.data![index]['Diasvigencia']) >= 90
+                              ? Colors.blueAccent
+                              : (snapshot.data![index]['Diasvigencia']) >= 60
+                                  ? Colors.greenAccent
+                                  : (snapshot.data![index]['Diasvigencia']) >=
+                                          30
+                                      ? Colors.yellowAccent
+                                      : Colors.redAccent,
+
+                          indicatorXY: 0.5,
+                          padding: const EdgeInsets.all(8),
+                          iconStyle: IconStyle(
+                              color: Colors.white,
+                              //iconData: Icons.access_alarm,
+                              iconData:
+                                  (snapshot.data![index]['Diasvigencia']) >= 90
+                                      ? Icons.alarm_on
+                                      : (snapshot.data![index]
+                                                  ['Diasvigencia']) >=
+                                              60
+                                          ? Icons.alarm
+                                          : (snapshot.data![index]
+                                                      ['Diasvigencia']) >=
+                                                  30
+                                              ? Icons.access_alarm
+                                              : Icons.access_time),
+                        ),
+                        beforeLineStyle: const LineStyle(
+                          color: Colors.indigo,
+                        ),
+                        afterLineStyle: const LineStyle(color: Colors.indigo),
+                        /***/
+                        endChild: Container(
+                          margin: const EdgeInsets.only(bottom: 12.0),
+                          decoration: BoxDecoration(
+                            //color: Colors.white70,
+                            color: (snapshot.data![index]['status'] != null &&
+                                    snapshot.data![index]['status'] > 0)
+                                ? (snapshot.data![index]['Diasvigencia']) >= 90
+                                    ? Colors.blue
+                                    : (snapshot.data![index]['Diasvigencia']) >=
+                                            60
+                                        ? Colors.green
+                                        : (snapshot.data![index]
+                                                    ['Diasvigencia']) >=
+                                                30
+                                            ? Colors.yellow
+                                            : Colors.red
+                                : Colors.black54,
+
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 16.0, horizontal: 12.0),
+                          /***/
+
+                          child: Column(
+                            children: [
+                              Text(
+                                '${snapshot.data![index]['descricao']}',
+                                style: const TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '${snapshot.data![index]['categoria']}',
+                                style: const TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                      onPressed: () {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(const SnackBar(
+                                                content: Text(
+                                                    ': Acionado o envio de mensagems. . .  ')));
+                                      },
+                                      icon: const Icon(Icons.email),
+                                      color: Colors.white),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.tiktok),
+                                      color: Colors.white),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${snapshot.data![index]['infor']}'
+                                        .toString()
+                                        .trim(),
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Faltam ${snapshot.data![index]['Diasvigencia']} dias para o término da vigência que ocorrerá em ${snapshot.data![index]['vigencia']}',
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                });
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _refleh,
+        tooltip: 'Atualizar',
+        child: const Icon(Icons.refresh),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//===================================================//
+class BarradeBotoes extends StatelessWidget {
+  const BarradeBotoes({super.key});
+//===================================================//
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      height: 50,
+      child: ButtonBar(alignment: MainAxisAlignment.start, children: [
+        TextButton(
+            child: const Text('Confirmar'),
+            onPressed: () {
+              "";
+            }),
+        TextButton(
+            child: const Text('Cancelar'),
+            onPressed: () {
+              "";
+            }),
+        TextButton(
+            child: const Text('Reprogramar'),
+            onPressed: () {
+              "";
+            }),
+      ]),
+    );
+  }
+}
+
+class BotoesAcao extends StatelessWidget {
+  const BotoesAcao({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          IconButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text(': Acionado o envio de mensagems. . .  ')));
+              },
+              icon: const Icon(Icons.email),
+              color: Colors.white),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.tiktok),
+              color: Colors.white),
+        ]);
+  }
+}
+
+
+
+/*
+
+return Card(
                   clipBehavior: Clip.antiAlias,
                   color: Colors.indigo,
                   child: Column(
@@ -130,64 +337,4 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 );
               },
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _refleh,
-        tooltip: 'Atualizar',
-        child: const Icon(Icons.refresh),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-//===================================================//
-class BarradeBotoes extends StatelessWidget {
-  const BarradeBotoes({super.key});
-//===================================================//
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 400,
-      height: 50,
-      child: ButtonBar(alignment: MainAxisAlignment.start, children: [
-        TextButton(
-            child: const Text('Confirmar'),
-            onPressed: () {
-              "";
-            }),
-        TextButton(
-            child: const Text('Cancelar'),
-            onPressed: () {
-              "";
-            }),
-        TextButton(
-            child: const Text('Reprogramar'),
-            onPressed: () {
-              "";
-            }),
-      ]),
-    );
-  }
-}
-
-class Relogioalarme extends StatelessWidget {
-  const Relogioalarme({super.key});
-  @override
-  @override
-  Widget build(BuildContext context) {
-    return Row(mainAxisSize: MainAxisSize.min, children: [
-      IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.access_alarm),
-          color: Colors.black),
-    ]);
-  }
-}
+*/
